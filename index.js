3073482b9aa4267ce443b0883ddc1066c2978fd8
NatashaChabak <124881750+NatashaChabak@users.noreply.github.com>
2025-02-23 21:09:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,141 +1,80 @@
-// for MariaDB
-import {createPool} from "mariadb"
-const sourceHost = "maria.northeurope.cloudapp.azure.com"
-// const sourceHost = "localhost"
-const dbMariaUser = "testi"
-// const dbMariaUser = "jyri"
-const dbMariaPassword = "mariadb1"
-// const dbMariaPassword = "Salasana1"
-const dbMaria = "adbms"
-// const dbMaria = "testi"
-
-
-// for MongoDB
-import {MongoClient, ObjectId} from "mongodb"
-const destHost = 'localhost:27017'
-const dbAdmin = "mongoAdmin"
-const dbAdminPassword = "Salasana1"
-const authDb = "admin"
-const destConnString = `mongodb://${dbAdmin}:${dbAdminPassword}@${destHost}?authSource=${authDb}`
-const dbMongo = "testi"
-const dbMongoUser = "jyri"
-const dbMongoPassword = "Salasana1"
+import { MongoClient, ObjectId } from 'mongodb'
+const dbHost = "localhost:27017"
+const dbUser = "jyri"
+const dbPassword = "Salasana1"
+const dbName = "testi"
 const dataCollection = "data"
-const usersCollection = "user"
+const usersCollection = "users"
+const destConnString = `mongodb://${dbUser}:${dbPassword}@${dbHost}?authSource=${dbName}`
+const dbServer = new MongoClient(destConnString)
 
-const copyDataFromMariaToMongo = async () => {
-    const pool = createPool({
-        host: sourceHost,
-        user: dbMariaUser,
-        password: dbMariaPassword,
-        database: dbMaria
-    })
 
-    let conn
-    try {
-        conn = await pool.getConnection()
-        const users = await conn.query("SELECT * FROM users")
-        const data = await conn.query("SELECT * FROM data")
+let logonUsers = new Map();
 
-        createCollections(users, data)
-    } catch (err) {
-        throw err
-    } finally {
-        if(conn) await conn.close()
-        await pool.end()
-    }
+const openDbConn = async () => {
+  try {
+      await dbServer.connect();
+      return dbServer.db(dbName)
+  } catch (error) {
+      console.error("Failed to conencto to the database", error)
+      throw error;
+  }
 }
 
-const createCollections = async (usersData, dataData) => {
-    const dbServer = new MongoClient(destConnString)
-
-    try {
-        await dbServer.connect()
-        const db = dbServer.db(dbMongo)
-
-        const dbs = await db.admin().listDatabases()
-        if(dbs.databases.find(d => d.name === dbMongo))
-            await db.dropDatabase()
-
-        const users = db.collection( usersCollection, {
-            validator: {
-                $jsonSchema: {
-                    bsonType: "Object",
-                    required: ["username", "password"],
-                    properties: {
-                        username: {
-                            bsonType: "string",
-                            description: "must be a string and it is required"
-                        },
-                        password: {
-                            bsonType: "string",
-                            decription: "must be a string and it is required"
-                        }
-                    }
-                }
-            }
-        })
-        users.createIndex({"username": 1}, {unique: true})
-
-        let result = await users.insertMany(usersData)
-        console.log(`${result.insertedCount} users were inserted`)
-
-        const data = db.collection( dataCollection, {
-            validator: { $jsonSchema: {
-                bsonType: "object",
-                required: ["id", "Firstname", "Surname", "userid"],
-                properties: {
-                    _id: {
-                        bsonType: ObjectId,
-                        description: "Must contain unique hex value"
-                    },
-                    Firstname: {
-                        bsonType: "string",
-                        description: "must be a string and is required"
-                    },
-                    Surname: {
-                        bsonType: "string",
-                        description: "must be a string and is required"
-                    },
-                    userid: {
-                        bsonType: "string",
-                        description: "must be a string and it should be in users colelction, too"
-                    },
-                }
-            }}
-        })
-
-        const processedData = await dataData.map(doc => {
-            return {
-                _id: ObjectId.createFromHexString(doc.id.toString(16).padStart(24,'0')),
-                Firstname: doc.Firstname,
-                Surname: doc.Surname,
-                userid: doc.userid
-            }
-        })
-        
-        console.log(processedData)
-
-        result = await data.insertMany(processedData)
-        console.log(`${result.insertedCount} data records were inserted`)
-        
-        const userExist = await db.command({ usersInfo: dbMongoUser})
-        if(!userExist.users[0]) {
-            result = await db.command({
-                createUser: dbMongoUser,
-                pwd: dbMongoPassword,
-                roles: [{role: 'readWrite', db: dbMongo}]
-            })
-            console.log("User created successfully", result)
-        } else
-            console.log("User", userExist.users[0].user, "already exist with roles:", 
-                        userExist.users[0].roles)
-    } catch (e) {
-        console.log(e)
-    } finally {
-        await dbServer.close()
-    }
-
+// Method for using a certain collection
+const connDbCollection = async (collection) => {
+  return db.collection(collection)
 }
 
-copyDataFromMariaToMongo()
+
+const sendQuery = async (query, toArray = false) => {
+  try {
+      const result = await query
+      if(toArray)
+          return await result.toArray()
+      else 
+          console.log("should do something")
+  } catch (err) {
+      console.error("Query execution failed", err)
+      throw err
+  }
+}
+
+// Function to test INNER JOIN query
+const getUserRecordCounts = async () => {
+    const usersCol = await connDbCollection(usersCollection);
+
+    return sendQuery(
+        usersCol.aggregate([
+            {
+                $lookup: {
+                    from: "data",
+                    localField: "username",
+                    foreignField: "userid",
+                    as: "user_data"
+                }
+            },
+            {
+                $match: {
+                    "user_data.0": { $exists: true } // Inner join: filters users without records
+                }
+            },
+            {
+                $project: {
+                    username: 1,
+                    users_records: { $size: "$user_data" },
+                    _id: 0
+                }
+            }
+        ]),
+        true
+    );
+};
+
+// Run the function
+getUserRecordCounts()
+    .then(() => process.exit(0)) // Exit after execution
+    .catch(err => {
+        console.error(err);
+        process.exit(1);
+    });
