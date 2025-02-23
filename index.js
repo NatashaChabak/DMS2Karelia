52e8c0eed982a62e0ed73406c3267c9fe346f9fb
NatashaChabak <124881750+NatashaChabak@users.noreply.github.com>
2025-02-23 20:55:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,87 @@
-import { MongoClient, ObjectId } from 'mongodb'
-const dbHost = "localhost:27017"
-const dbUser = "jyri"
-const dbPassword = "Salasana1"
-const dbName = "testi"
+// for MariaDB
+import {createPool} from "mariadb"
+const sourceHost = "maria.northeurope.cloudapp.azure.com"
+const dbMariaUser = "testi"
+const dbMariaPassword = "mariadb1"
+const dbMaria = "adbms"
+
+// for MongoDB
+import {MongoClient, ObjectId} from "mongodb"
+const destHost = 'localhost:27017'
+const dbAdmin = "mongoAdmin"
+const dbAdminPassword = "Salasana1"
+const authDb = "admin"
+const destConnString = `mongodb://${dbAdmin}:${dbAdminPassword}@${destHost}?authSource=${authDb}`
+const dbMongo = "testi"
+const dbMongoUser = "jyri"
+const dbMongoPassword = "Salasana1"
 const dataCollection = "data"
-const usersCollection = "users"
-const destConnString = `mongodb://${dbUser}:${dbPassword}@${dbHost}?authSource=${dbName}`
-const dbServer = new MongoClient(destConnString)
+const usersCollection = "user"
 
-let logonUsers = new Map();
+const copyDataFromMariaToMongo = async () => {
+    const pool = createPool({
+        host: sourceHost,
+        user: dbMariaUser,
+        password: dbMariaPassword,
+        database: dbMaria
+    })
 
-// Method for connecting to the database
-const openDbConn = async () => {
+    let conn
     try {
-        await dbServer.connect();
-        return dbServer.db(dbName)
-    } catch (error) {
-        console.error("Failed to conencto to the database", error)
-        throw error;
+        conn = await pool.getConnection()
+        const users = await conn.query("SELECT * FROM users")
+        const data = await conn.query("SELECT * FROM data")
+
+        createCollections(users, data)
+    } catch (err) {
+        throw err
+    } finally {
+        if(conn) await conn.close()
+        await pool.end()
     }
 }
 
+const createCollections = async (usersData, dataData) => {
+    const dbServer = new MongoClient(destConnString)
 
-openDbConn();
+    try {
+        await dbServer.connect()
+        const db = dbServer.db(dbMongo)
+
+        const dbs = await db.admin().listDatabases()
+        if(dbs.databases.find(d => d.name === dbMongo))
+            await db.dropDatabase()
+
+        const users = db.collection( usersCollection, {
+            validator: {
+                $jsonSchema: {
+                    bsonType: "Object",
+                    required: ["username", "password"],
+                    properties: {
+                        username: {
+                            bsonType: "string",
+                            description: "must be a string and it is required"
+                        },
+                        password: {
+                            bsonType: "string",
+                            decription: "must be a string and it is required"
+                        }
+                    }
+                }
+            }
+        })
+        users.createIndex({"username": 1}, {unique: true})
+
+        let result = await users.insertMany(usersData)
+        console.log(`${result.insertedCount} users were inserted`)
+
+
+    } catch (e) {
+        console.log(e)
+    } finally {
+        await dbServer.close()
+    }
+
+}
+
+copyDataFromMariaToMongo()
