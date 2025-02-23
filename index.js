7171e14e53822de0aab65af3f1c44ea5df2a483e
NatashaChabak <124881750+NatashaChabak@users.noreply.github.com>
2025-02-23 21:27:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,80 +1,33 @@
-import { MongoClient, ObjectId } from 'mongodb'
-const dbHost = "localhost:27017"
-const dbUser = "jyri"
-const dbPassword = "Salasana1"
-const dbName = "testi"
+import {MongoClient, ObjectId} from "mongodb"
+const destHost = 'localhost:27017'
+const dbAdmin = "mongoAdmin"
+const dbAdminPassword = "Salasana1"
+const authDb = "admin"
+const destConnString = `mongodb://${dbAdmin}:${dbAdminPassword}@${destHost}?authSource=${authDb}`
+const dbMongo = "testi"
+const dbMongoUser = "jyri"
+const dbMongoPassword = "Salasana1"
 const dataCollection = "data"
-const usersCollection = "users"
-const destConnString = `mongodb://${dbUser}:${dbPassword}@${dbHost}?authSource=${dbName}`
-const dbServer = new MongoClient(destConnString)
+const usersCollection = "user"
 
+const listDatabases = async () => {
+  const dbServer = new MongoClient(destConnString)
 
-let logonUsers = new Map();
-
-const openDbConn = async () => {
   try {
       await dbServer.connect();
-      return dbServer.db(dbName)
-  } catch (error) {
-      console.error("Failed to conencto to the database", error)
-      throw error;
-  }
-}
+      
+      const databases = await dbServer.db().admin().listDatabases();
 
-// Method for using a certain collection
-const connDbCollection = async (collection) => {
-  return db.collection(collection)
-}
+      console.log("Databases:");
+      databases.databases.forEach(db => console.log(`- ${db.name}`));
 
-
-const sendQuery = async (query, toArray = false) => {
-  try {
-      const result = await query
-      if(toArray)
-          return await result.toArray()
-      else 
-          console.log("should do something")
   } catch (err) {
-      console.error("Query execution failed", err)
-      throw err
+      console.error("Error listing databases:", err);
+  } finally {
+      await client.close(); // Close connection
   }
-}
-
-// Function to test INNER JOIN query
-const getUserRecordCounts = async () => {
-    const usersCol = await connDbCollection(usersCollection);
-
-    return sendQuery(
-        usersCol.aggregate([
-            {
-                $lookup: {
-                    from: "data",
-                    localField: "username",
-                    foreignField: "userid",
-                    as: "user_data"
-                }
-            },
-            {
-                $match: {
-                    "user_data.0": { $exists: true } // Inner join: filters users without records
-                }
-            },
-            {
-                $project: {
-                    username: 1,
-                    users_records: { $size: "$user_data" },
-                    _id: 0
-                }
-            }
-        ]),
-        true
-    );
 };
 
-// Run the function
-getUserRecordCounts()
-    .then(() => process.exit(0)) // Exit after execution
-    .catch(err => {
-        console.error(err);
-        process.exit(1);
-    });
+
+listDatabases();
+
